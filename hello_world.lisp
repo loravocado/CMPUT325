88c8c6f4e38da03fa25207970e3cafe71c27133c
Lora <lora@ualberta.ca>
2023-01-20 18:19:41 -0700

    1-3

--- hello_world.lisp
+++ hello_world.lisp
@@ -11,3 +11,41 @@
     )
 )
 
+;QUESTION 2
+;documentation
+
+(defun xflatten (x)
+    (if (null x)
+        '()
+        (if (atom (car x))
+            (cons (car x) (xflatten (cdr x)))
+            (append (xflatten (car x)) (xflatten (cdr x)))
+        )
+    )
+)
+
+;QUESTION 3
+;documentation
+
+(defun check_exists (A B)
+    (if (null B)
+        nil
+        (if (eq A (car B))
+            t 
+            (check_exists A (cdr B))
+        )
+    )
+)
+
+(defun remove-duplicate (x) 
+    (if (null x)
+        x
+        (if (null (check_exists (car x) (cdr x)))
+            (cons (car x) (remove-duplicate(cdr x)))
+            (remove-duplicate(cdr x))
+        )
+    )
+)
+
+;QUESTION 4
+;documentation
